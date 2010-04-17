0f1e45b0afb311db258139f6fe419f6284e37252
Rafa G <rgo@aspgems.com>
2010-04-17 17:05:49 +0200

    Fetcher de encimera de inducción

--- app/models/worktop_induction.rb
+++ app/models/worktop_induction.rb
@@ -21,11 +21,32 @@
 #
 class WorktopInduction < ActiveRecord::Base
 
+  include DataFetcher
+  data_fetcher :uri => 'http://www.idae.es/index.php/mod.buscador/mem.fbusquedaEncimerasInduccion/relmenu.87',
+               :form => 'form_buscar_elect',
+               :selector => 'div#dts-lst.con table.tbl-f3 tbody tr'
+
   define_indexes do
     indexes producer
     indexes product
     indexes brand
     indexes model
+  end
+
+  def self.fetch_mapping
+    {:prodtr => 'producer',
+     :prod => 'product',
+     :marc => 'brand',
+     :model => 'model',
+     :tip => 'type',
+     :numfoct => 'total_torchs',
+     :numfoci => 'inductio_torchs',
+     :anche => 'width_ext',
+     :fonde => 'deep_ext',
+     :anchi => 'width_ins',
+     :fondi => 'deep_ins',
+     :potencr => 'power_torchs',
+     :potenci => 'power_induction_torchs'}
   end
 end
 
