c23cfccfc8cb8be396940b8098ee7858f78f6bd4
Javier Lafora <javierlafora@gmail.com>
2010-04-17 18:36:42 +0200

    Fetcher for cars

--- db/migrate/20100417002554_create_cars.rb
+++ db/migrate/20100417002554_create_cars.rb
@@ -3,10 +3,24 @@ class CreateCars < ActiveRecord::Migration
     create_table :cars do |t|
       t.string  :brand
       t.string  :model
-      t.boolean :diesel, :default => false
-      t.decimal :consumption, :precision => 10, :scale => 2 
-      t.integer :emission, :default => 0
+      t.string  :finish
+      t.string  :fuel
+      t.decimal :consume, :precision => 10, :scale => 2 
+      t.integer :emissions, :default => 0
       t.string  :rating
+      t.string  :market_segment
+      t.integer :engine
+      t.integer :cylinders
+      t.string  :wheel_drive
+      t.string  :gear
+      t.string  :minimal_tare
+      t.decimal :power, :precision => 10, :scale => 2
+      t.integer :torque
+      t.integer :length
+      t.integer :width
+      t.integer :height
+      t.integer :seats
+
       t.timestamps
     end
   end
