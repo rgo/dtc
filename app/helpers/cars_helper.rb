2187243f5c38b9d7be856f36a43ad8b04bfe26ae
Javier Lafora <javierlafora@gmail.com>
2010-04-17 23:34:58 +0200

    added helper for creating consume range selecting

--- app/helpers/cars_helper.rb
+++ app/helpers/cars_helper.rb
@@ -1,2 +1,12 @@
 module CarsHelper
+  def consume_range_options_for_car
+    [
+      [t('general.range.less', :max => 5), Car::LESS_THAN_5],
+      [t('general.range.between', :min => 5, :max => 6), Car::BETWEEN_5_AND_6],
+      [t('general.range.between', :min => 6, :max => 7), Car::BETWEEN_6_AND_7],
+      [t('general.range.between', :min => 7, :max => 9), Car::BETWEEN_7_AND_9],
+      [t('general.range.greater', :min => 9), Car::GREATER_THAN_9]
+    ]
+  end
+
 end
