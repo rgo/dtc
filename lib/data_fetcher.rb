65d7f3425bc121b33a7c09fe0d95e4dd504ccae5
Paco Guzman <fjguzman@aspgems.com>
2010-04-17 11:57:10 +0200

    First fetcher

--- lib/data_fetcher.rb
+++ lib/data_fetcher.rb
@@ -10,29 +10,33 @@ module DataFetcher
   module ClassMethods
 
     def data_fetcher(options={})
-      cattr_accessor :uri_data_fetcher, :form_data_fetcher, :search_selector, :fetch_mapping
+      cattr_accessor :uri_data_fetcher, :form_data_fetcher, :search_selector, :agent
       self.uri_data_fetcher = options[:uri]
       self.form_data_fetcher = options[:form]
       self.search_selector = options[:selector]
-      self.fetch_mapping = options[:fetch_mapping] || {}
+      self.agent = Mechanize.new
     end
 
     def fetch
-      agent = Mechanize.new
+      inserts = 0
 
-      agent.get(self.uri_data_fetcher)
-      form = agent.page.form(self.form_data_fetcher)
+      agent.get(uri_data_fetcher)
+      form = agent.page.form(form_data_fetcher)
       doc = form.submit
 
       begin
-        doc.search(self.search_selector).each do |row|
+        doc.search(search_selector).each do |row|
           object = self.new
-          self.fetch_mapping.each do |value, method|
-            row.search("td[@headers='#{value}']").each do |data|
-              object.send("#{method}=", data.text)
+          self.fetch_mapping.each do |key, method|
+            row.search("td[@headers='#{key.to_s}']").each do |data|
+              if method.is_a?(Array)
+                object.send("#{method.first}=", method.last.call(data.text))
+              else
+                object.send("#{method}=", data.text)
+              end
             end
           end
-          object.save!
+          inserts += 1 if object.save!
         end
       end while (agent.page.link_with(:text => 'Siguiente >') && doc = agent.page.link_with(:text => 'Siguiente >').click)
     end
