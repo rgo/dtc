ab209894fd349aba269cc7faafa5894fcb4cea32
Jorge Alvarez Moreno <jorge@macbook-pro-de-jorge-alvarez-moreno.local>
2010-04-18 01:27:03 +0200

    plugin de stickies para presentar informacion

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -13,7 +13,7 @@ class CarsController < ApplicationController
   # GET /cars/1
   # GET /cars/1.xml
   def show
-    @car = Car.find(params[:id])
+    #@car = Car.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
