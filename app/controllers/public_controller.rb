608ac317273a3bde5ef5ef736ab92fbd4041ceac
Jorge Alvarez Moreno <jorge@macbook-pro-de-jorge-alvarez-moreno.local>
2010-04-18 20:20:54 +0200

    El calvo de la sexta no sabe menear el bullarengue

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -3,5 +3,10 @@ class PublicController < ApplicationController
     @cars = Car.best_cars
     
   end
+  
+  def about_us
+    @meta_title       = "El equipo Abredatos que ha creado Desenchufa tu Casa"
+    @meta_description = "Abredatos es un concurso que busca conciencar sobre la necesidad de que las administraciones liberen datos de forma estándar y accesible a todos los ciudadanos."
+  end
 
 end
