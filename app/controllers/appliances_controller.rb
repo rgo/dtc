cb90181b7e356e47ffed95654c2b24bb040ad391
Paco Guzman <fjguzman@aspgems.com>
2010-04-19 00:24:05 +0200

    Fixes in extrem

--- app/controllers/appliances_controller.rb
+++ app/controllers/appliances_controller.rb
@@ -12,14 +12,14 @@ class AppliancesController < ApplicationController
   end
 
   def search
-    @dishwashers = Dishwasher.search(params[:q], :page => 1, :per_page => 20)
-    @dryers = Dryer.search(params[:q], :page => 1, :per_page => 20)
-    @freezers = Freezer.search(params[:q], :page => 1, :per_page => 20)
-    @fridges = Fridge.search(params[:q], :page => 1, :per_page => 20)
-    @fridge_freezers = FridgeFreezer.search(params[:q], :page => 1, :per_page => 20)
-    @ovens = Oven.search(params[:q], :page => 1, :per_page => 20)
-    @washer_dryers = WasherDryer.search(params[:q], :page => 1, :per_page => 20)
-    @washing_machines = WashingMachine.search(params[:q], :page => 1, :per_page => 20)
+    @dishwashers = Dishwasher.brand_or_model_like(params[:q]).paginate(:page => 1, :per_page => 20)
+    @dryers = Dryer.brand_or_model_like(params[:q]).paginate(:page => 1, :per_page => 20)
+    @freezers = Freezer.brand_or_model_like(params[:q]).paginate(:page => 1, :per_page => 20)
+    @fridges = Fridge.brand_or_model_like(params[:q]).paginate(:page => 1, :per_page => 20)
+    @fridge_freezers = FridgeFreezer.brand_or_model_like(params[:q]).paginate(:page => 1, :per_page => 20)
+    @ovens = Oven.brand_or_model_like(params[:q]).paginate(:page => 1, :per_page => 20)
+    @washer_dryers = WasherDryer.brand_or_model_like(params[:q]).paginate(:page => 1, :per_page => 20)
+    @washing_machines = WashingMachine.brand_or_model_like(params[:q]).paginate(:page => 1, :per_page => 20)
   end
 
 end
