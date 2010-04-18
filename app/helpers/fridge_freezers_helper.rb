0e906dda52b83d626c89156a2b4ee2fbc0819cb9
Paco Guzman <fjguzman@aspgems.com>
2010-04-18 13:43:28 +0200

    Fridge freezers

--- app/helpers/fridge_freezers_helper.rb
+++ app/helpers/fridge_freezers_helper.rb
@@ -1,2 +1,12 @@
 module FridgeFreezersHelper
+
+  def consume_range_options_for_fridge_freezer
+    [
+        [t('fridge_freezers.index.consume_range.less', :max => 300), '1'],
+        [t('fridge_freezers.index.consume_range.between', :min => 300, :max => 400), '2'],
+        [t('fridge_freezers.index.consume_range.between', :min => 400, :max => 500), '3'],
+        [t('fridge_freezers.index.consume_range.greater', :min => 500), '4']
+    ]
+  end
+
 end
