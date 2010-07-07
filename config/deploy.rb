d58bed314a53c391dda6c3eb6bbd7365476308ea
Javier Lafora <javierlafora@gmail.com>
2010-07-07 20:00:35 +0200

    changed deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,35 +1,83 @@
 set :application, "dtc"
 set :repository,  "git://github.com/rgo/dtc.git"
-
-set :user, "dtc"
+set :user, application
+ssh_options[:config] = false
 set :use_sudo, false
+set :keep_releases, 2
+set :rails_env, "production"
+set :latest_tag, ""
 
+set(:scm_username) { Capistrano::CLI.ui.ask("Type is your svn username: ") }
+set(:scm_password){ Capistrano::CLI.password_prompt("Type your svn password for user #{scm_username}: ") }
 
 set :scm, :git
-set :deploy_to, "/home/#{user}/app/"
 set :branch, "master"
-
 set :deploy_via, :remote_cache
+set :deploy_to, "/home/#{user}/app"
 
-##
-# Database access data variables
-#
-set :database_server, "localhost"
-set :database_username, "dtc"
-set (:database_password){ Capistrano::CLI.password_prompt("Database password: ") }
-set :database_name_dev, "dtc_dev"
-set :database_name_test, "dtc_test"
-set :database_name_prd, "dtc_production"
+role :app, "megatron.aspgems.com"
+role :web, "megatron.aspgems.com"
+role :db,  "megatron.aspgems.com", :primary => true
 
-##
-# Config_files & asset directories
-#
-set :config_files_array, %w(database.yml)
-set :assets_array, []
+config_files=%w(database.yml sphinx.yml)
+initializers=%w()
+dirs=%w(config)
+after "deploy:update_code","deploy:symlink_config"
+after "deploy:setup","deploy:create_dirs"
+after "deploy:symlink_config","deploy:symlink_initializers"
 
-role :web, "vapor.aspgems.com"                          # Your HTTP server, Apache/etc
-role :app, "vapor.aspgems.com"                          # This may be the same as your `Web` server
-role :db,  "vapor.aspgems.com", :primary => true # This is where Rails migrations will run
-role :db,  "vapor.aspgems.com"
+after "deploy","deploy:cleanup"
+# Here comes the app config
 
-load 'config/deploy/common_tasks.rb'
+namespace :deploy do
+
+  task :symlink_config, :roles => :app do
+    config_files.each do |f|
+      run "ln -nfs #{shared_path}/config/#{f} #{release_path}/config/#{f}"
+    end
+  end
+
+  task :symlink_initializers, :roles => :app do
+    initializers.each do |f|
+      run "ln -nfs #{shared_path}/config/#{f} #{release_path}/config/initializers/#{f}"
+    end
+  end
+
+  task :restart, :roles => :app do
+    run "touch  #{current_path}/tmp/restart.txt"
+  end
+
+  task :create_dirs, :roles => :app do
+    dirs.each do |d|
+      run "mkdir -p #{shared_path}/#{d}"
+    end
+  end
+
+  desc "Enables maintenance mode in the app"
+  task :maintenance_on, :roles => :app do
+    run "cp #{shared_path}/system/maintenance.html.disabled #{current_path}/public/system/maintenance.html"
+  end
+
+  desc "Disables maintenance mode in the app"
+  task :maintenance_off, :roles => :app do
+    run "rm #{shared_path}/system/maintenance.html"
+  end
+end
+
+# Here comes the application namespace for custom tasks
+
+namespace application do
+
+end
+
+# Bundler tasks for new applications
+# after "deploy:symlink_initializers", "bundler:bundle"
+namespace :bundler do
+  task :bundle, :roles => :app do
+    run "cd #{release_path} && bundle lock"
+  end
+
+  task :bundle_new_release do
+    run "cd #{current_path} && bundle install"
+  end
+end
